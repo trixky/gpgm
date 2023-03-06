a420f77f6beca8b0589db08a908d52fed9d74a29
trixky <mathisbois.dev@gmail.com>
2023-03-06 09:21:39 +0100

    [Dev] visual: fix the slow visualization by printing only the new last generation

--- client/src/components/visual/visual.svelte
+++ client/src/components/visual/visual.svelte
@@ -4,6 +4,7 @@
 	import GenerationStore from '../../stores/generation';
 	import { get_color_from_percentage } from '../../utils/color';
 	import type GenerationModel from '../../models/generation';
+	import Config from '../../config';
 
 	const PIXEL_SIZE = 4;
 	const COLOR_R = 0;
@@ -11,81 +12,98 @@
 	const COLOR_B = 2;
 	const COLOR_X = 3;
 
-	let mounted = false;
+
 
 	let previous_frame: number;
 	export let frame: number;
 
-    let width = 0
-    let height = 0
-    
-	$: width = $GenerationStore.length ? $GenerationStore[0].instances.length : 0;
-	$: height = $GenerationStore.length;
+	let mounted = false;
+
+	let width = 0;
+	let height = 0;
 
 	let canvas: any = undefined;
 	let ctx: any = undefined;
 
-    function handle_bottom() {
+	function handle_bottom() {
 		window.scrollTo({
-			top: document.body.scrollHeight,
+			top: document.body.scrollHeight
 		});
 	}
 
 	const draw = (generations: Array<GenerationModel>) => {
 		// https://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
 
-		if (width) {
-			var image = ctx.getImageData(0, 0, width, height);
+		const local_height = $GenerationStore.length;
+
+		if (local_height) {
+			const last_generation = generations[generations.length - 1];
+			const local_width = last_generation.instances.length;
+
+			var image = ctx.getImageData(0, 0, local_width, local_height);
 			var pixels = image.data;
 
-			let off = 0;
+			let pixel = (local_height - 3) * local_width * 4;
 
-			generations.forEach((generation) => {
-				generation.instances.forEach((instance) => {
-					const rgb = get_color_from_percentage(instance.score);
+			last_generation.instances.forEach((instance) => {
+				const rgb = get_color_from_percentage(instance.score);
 
-					pixels[off + COLOR_R] = rgb.red;
-					pixels[off + COLOR_G] = rgb.green;
-					pixels[off + COLOR_B] = rgb.blue;
+				pixels[pixel + COLOR_R] = rgb.red;
+				pixels[pixel + COLOR_G] = rgb.green;
+				pixels[pixel + COLOR_B] = rgb.blue;
 
-					pixels[off + COLOR_X] = 255;
+				pixels[pixel + COLOR_X] = 255;
 
-					off += PIXEL_SIZE;
-				});
+				pixel += PIXEL_SIZE;
 			});
 
 			ctx.putImageData(image, 0, 0);
+			width = local_width;
+			height = generations.length;
+		} else {
+			// reset
+			height = 0;
 		}
 	};
 
 	onMount(async () => {
 		mounted = true;
+
+		ctx = canvas.getContext('2d', {
+			willReadFrequently: true // browser optimization
+		});
+		canvas.height = Config.io.generations.max;
 	});
 
 	$: if (mounted && previous_frame != frame && canvas != undefined) {
 		(async () => {
 			previous_frame = frame;
 
-			ctx = canvas.getContext('2d');
-			canvas.width = width;
-			canvas.height = height;
-            handle_bottom()
+			ctx = canvas.getContext('2d', {
+				willReadFrequently: true
+			});
+
+			if (canvas.width != width) {
+				canvas.width = width;
+			}
+
+			handle_bottom();
 			await tick();
 			draw($GenerationStore);
-            handle_bottom()
+			handle_bottom();
 		})();
 	}
 </script>
 
 <!-- ---------------------------------------------- CONTENT -->
-<div class="visual shadow" class:invisible={!width}>
-    <canvas bind:this={canvas} />
+<div class="visual shadow" class:invisible={!height} style="width: {width}px; height: {height}px;">
+	<canvas bind:this={canvas} />
 </div>
 
 <!-- ---------------------------------------------- STYLE -->
 <style lang="postcss">
 	.visual {
-		@apply m-auto w-fit mt-8 mb-3;
+		@apply m-auto w-fit mt-8 mb-3 overflow-hidden;
 		border: solid 1px black;
 	}
 </style>
