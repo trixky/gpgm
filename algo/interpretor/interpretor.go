b75f2f84ac91db562fc148e1a221305751709163
trixky <mathisbois.dev@gmail.com>
2023-03-07 15:18:33 +0100

    [Dev] interpretor: add the gene interpretor of the algo

---
+++ algo/interpretor/interpretor.go
@@ -0,0 +1,41 @@
+package interpretor
+
+import (
+	"github.com/trixky/krpsim/algo/core"
+	"github.com/trixky/krpsim/algo/instance"
+)
+
+func Interpret(i instance.Instance, initial_context core.SimulationInitialContext, stock core.Stock) (processes []core.Process) {
+	i_genes_cpy := make([]instance.Gene, len(i.Chromosome.Genes))
+	processes_order := []core.Process{}
+	copy(i_genes_cpy, i.Chromosome.Genes)
+
+	for len(i_genes_cpy) > 0 {
+		var best_value uint16 = 0
+		var best_index int = 0
+
+		for index, gene := range i_genes_cpy {
+			if gene.Value > best_value {
+				best_value = gene.Value
+				best_index = index
+			}
+		}
+
+		i_genes_cpy = append(i_genes_cpy[:best_index], i_genes_cpy[best_index+1:]...)
+		processes_order = append(processes_order, initial_context.Processes[best_index])
+	}
+
+	execution := true
+
+	for execution {
+		execution = false
+		for _, process := range processes_order {
+			if ok := process.Execute(&stock); ok {
+				execution = true
+				processes = append(processes, process)
+			}
+		}
+	}
+
+	return
+}
---
+++ algo/interpretor/interpretor_test.go
@@ -0,0 +1,68 @@
+package interpretor
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/trixky/krpsim/algo/core"
+	"github.com/trixky/krpsim/algo/instance"
+)
+
+func TestInterpretor(t *testing.T) {
+	stock := core.Stock{
+		"cat": 3,
+		"dog": 0,
+		"pig": 0,
+	}
+
+	initial_context := core.SimulationInitialContext{
+		Stock: core.Stock{
+			"cat": 5,
+			"dog": 0,
+			"pig": 0,
+		},
+		Processes: []core.Process{
+			{
+				Name: "111",
+				Inputs: map[string]int{
+					"cat": 1,
+				},
+				Outputs: map[string]int{
+					"pig": 1,
+				},
+				Delay: 100,
+			},
+			{
+				Name: "222",
+				Inputs: map[string]int{
+					"cat": 1,
+				},
+				Outputs: map[string]int{
+					"pig": 10,
+				},
+				Delay: 100,
+			},
+		},
+		Optimize: map[string]bool{
+			"cat": false,
+		},
+	}
+
+	instance := instance.Instance{
+		Chromosome: instance.Chromosome{
+			Genes: []instance.Gene{
+				{Value: 1},
+				{Value: 2},
+			},
+		},
+	}
+
+	processes := Interpret(instance, initial_context, stock)
+
+	fmt.Println(len(processes))
+	fmt.Println(stock)
+
+	if len(processes) != 3 {
+		t.Fatalf(`expected = %d, got = %d`, 3, len(processes))
+	}
+}
