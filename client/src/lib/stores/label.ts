7517b8c5c29c2762f462dcad7f2c903bcd13c26e
trixky <mathisbois.dev@gmail.com>
2023-03-17 09:22:34 +0100

    [Fix] chart: increment by one the label of generation number

--- client/src/lib/stores/label.ts
+++ client/src/lib/stores/label.ts
@@ -4,6 +4,6 @@ import { derived } from 'svelte/store';
 export default derived(
 	InstanceStore,
 	$InstanceStore => $InstanceStore.length > 0 ?
-    $InstanceStore[0].scores.map((_, index) => index.toString())
+    $InstanceStore[0].scores.map((_, index) => (index + 1).toString())
     : []
 );
